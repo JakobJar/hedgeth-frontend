64a37d30f0c638ccd6e0f573c65c4c65e209fb5c
Jakob <jakob.stiel@gmail.com>
2023-09-19 13:02:20 +0200

    Add homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <NuxtLayout name="main">
-
+    <Toolbar>
+      <template #start>
+        <Dropdown label="Show" v-model="selectedFilter" :options="filter" option-label="name"/>
+      </template>
+      <template #end>
+        <Button label="Create Fund" severity="success"/>
+      </template>
+    </Toolbar>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-
+const selectedFilter = ref();
+const filter = ref([
+  { name: 'Show all funds', value: 'all' },
+  { name: 'Show open funds', value: 'open' },
+  { name: 'Show raising funds', value: 'raising' }
+]);
 </script>
 
 <style scoped>
