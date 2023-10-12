2b0ef75b376e7ef4fc6a3fd5f569b9b1b4317c35
Jakob <jakob.stiel@gmail.com>
2023-10-12 11:44:22 +0200

    Add router example

--- server/api/uniswap-route/index.get.ts
+++ server/api/uniswap-route/index.get.ts
@@ -1,6 +1,6 @@
 import {PrismaClient} from "@prisma/client";
-import {AlphaRouter} from "@uniswap/smart-order-router";
-import {ChainId} from "@uniswap/sdk-core";
+import {AlphaRouter, SwapType} from "@uniswap/smart-order-router";
+import {ChainId, CurrencyAmount, Percent, Token, TradeType} from "@uniswap/sdk-core";
 import {JsonRpcProvider} from "ethers";
 import {useEthersProvider} from "~/composables/useEthers";
 
@@ -20,5 +20,52 @@ export default defineEventHandler(async (event) => {
         provider: provider,
     });
 
-    return "Hallo";
+    const FLOKI = new Token(
+        1,
+        '0xcf0c122c6b73ff809c693db761e7baebe62b6a2e',
+        9,
+        'FLOKI',
+        'FLOKI INU'
+    );
+
+    const WETH = new Token(
+        ChainId.SEPOLIA,
+        '0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9',
+        18,
+        'WETH',
+        'Wrapped Ether'
+    );
+
+    const USDC = new Token(
+        ChainId.SEPOLIA,
+        '0x6f14C02Fc1F78322cFd7d707aB90f18baD3B54f5',
+        6,
+        'USDC',
+        'USD//C'
+    );
+
+
+
+    const options = {
+        recipient: '',
+        slippageTolerance: new Percent(1, 100),
+        deadline: Math.floor(Date.now() / 1000 + 1800),
+        type: SwapType.SWAP_ROUTER_02,
+    }
+    const typedValueParsed = '1000000000000000000'
+
+
+
+
+    const route = await router.route(
+        CurrencyAmount.fromRawAmount(
+            WETH,
+            typedValueParsed.toString()
+        ),
+        USDC,
+        TradeType.EXACT_INPUT,
+        options
+    )
+
+    return route;
 });
